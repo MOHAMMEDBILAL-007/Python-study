69ed30c5b01228b373f6726efa79c0be51002a74
MOHAMMEDBILAL-007 <mohammedbilalsharief99@gmail.com>
2025-11-06 00:45:15 +0530

    just another day

--- self learning/Pandas/exp.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-df = pd.read_csv("D:\learning\Python-study\self learning\Pandas\data.csv")
-for i in df.columns:
-    for j in df[i]:
-        if j == None :
-            print("null")
