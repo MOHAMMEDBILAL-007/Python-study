3d57daebc895eeeee07039fe0f0c48772459ddb8
MOHAMMEDBILAL-007 <mohammedbilalsharief99@gmail.com>
2025-11-19 16:47:35 +0530

    just another day

--- self learning/python_test.py
+++ self learning/python_test.py
@@ -1,4 +1,3 @@
-f = open("D:\learning\Python-study\self learning\manage 49.txt",'r')
-for i in f.readlines():
-    L1 = list(j for j in list(i.split(" ")) if j.strip())
-    print(L1)
+x = lambda sj : sj*"*"
+
+print(x(12))
