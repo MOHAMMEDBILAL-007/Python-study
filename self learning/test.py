65bb86b32a62a3e55056c10db0268a37436c615b
MOHAMMEDBILAL-007 <mohammedbilalsharief99@gmail.com>
2026-01-28 18:09:15 +0530

    revise

---
+++ self learning/test.py
@@ -0,0 +1,39 @@
+# print("hello","heha","kjsdbhcidsj",sep=" .. ")
+# c = 1+2j
+# print(type(c))
+# st = """dowhat
+# everyou
+# wanti
+# dontcare"""
+# print(st)
+# # for i in st:
+# #     print(i)
+# st ="hsdgcuuhsdhsjaihdoaisdiahsdhsdakjkjdhajska"
+# print(st[3])
+# print(len(st))
+# print(st[:])
+
+# import time
+# print(time.time())
+# print(time.ctime())
+# print(time.localtime())
+# print(time.gmtime())
+# hour = time.strftime("%H")
+# minute = time.strftime("%M")
+# seconds= time.strftime("%S")
+# hour = int(hour)%12
+# print(hour,":",int(minute),":",int(seconds))
+
+
+# import time
+# sttime = time.time()
+# x = 0
+# for i in range(1000):
+#     x+=i
+# print(x)
+# print(time.time()-sttime)
+# i = 10
+# while True:
+#     print(i)
+#     if i >5:
+#         break
