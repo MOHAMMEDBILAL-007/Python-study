fec8a0eae7bde3d391206f6d0245bccf847dc99b
MOHAMMEDBILAL-007 <mohammedbilalsharief99@gmail.com>
2026-02-15 00:24:07 +0530

    n

--- self learning/python58.py
+++ self learning/python58.py
@@ -21,7 +21,7 @@ user1 = user()#here the __init__ is called the moment this object is created
 # with this constructor we can initialize the values as arguments while creating the object
 
 class player():
-    def __init__(self,name, occupation,networth):# this method helps the class take the values as arguments while creating the object of the class
+    def __init__(self,name, occupation,networth):# this method helps the class take the values as arguments while creating the object of the class player
         self.name = name
         self.occupation = occupation
         self.networth = networth
@@ -34,3 +34,16 @@ player1.display()
 
 player2 = player("boruto","usless trash",10000)
 player2.display()
+
+class anime:
+    def __new__(cls,*args,**kwargs):
+        return super().__new__(cls)
+    def __init__(self,name,mc,mp):
+        self.name = name
+        self.mc = mc
+        self.mp = mp
+    def arc(self,antoganist,arcname,vp):
+        print(" In the verse of ",self.name," the Mc ",self.mc," with his pwer level ",self.mp," is currently fighting",antoganist," his power level is",vp,"in the",arcname)
+
+an1 = anime("bleach","ichigo",100000)
+an1.arc("yuhawahac","thousand years of blood war",1000000)
