51552ba64a4428167edee37654c80134dc9858a1
MOHAMMEDBILAL-007 <mohammedbilalsharief99@gmail.com>
2026-04-04 22:41:42 +0530

    jj

--- self learning/python33.py
+++ self learning/python33.py
@@ -1,23 +1,47 @@
-#dictionaries
-dict = {"name":"joe", "age":23,"id":2005,"country":"america"}
-print(dict) # prints the whole dictionary
-print(dict.keys())# prints only the keys of the dictionary
-print(dict.values())# prints only the values in the dictionary
-print(dict.items())# prints the key value pairs
+# #dictionaries
+# dict = {"name":"joe", "age":23,"id":2005,"country":"america"}
+# print(dict) # prints the whole dictionary
+# print(dict.keys())# prints only the keys of the dictionary
+# print(dict.values())# prints only the values in the dictionary
+# print(dict.items())# prints the key value pairs
 
-# accessing the elements through keys
-print(dict["name"])# if the key doesent exist then it throws an error
-print(dict["age"])
-#or
-print(dict.get("name"))#  if the key doesent exist it returns "none"
-for i in dict:# by default dictionary is iterated over keys 
-    print(i)# this only prints the keys
-    print(dict[i])# this only prints values
+# # accessing the elements through keys
+# print(dict["name"])# if the key doesent exist then it throws an error
+# print(dict["age"])
+# #or
+# print(dict.get("name"))#  if the key doesent exist it returns "none"
+# for i in dict:# by default dictionary is iterated over keys 
+#     print(i)# this only prints the keys
+#     print(dict[i])# this only prints values
 
-for i in dict.keys():# here intentionally iterated over keys
-    print(i)
-for i in dict.values():# here intentionally iterated over values
-    print(i)
+# for i in dict.keys():# here intentionally iterated over keys
+#     print(i)
+# for i in dict.values():# here intentionally iterated over values
+#     print(i)
 
-for i,j in dict.items():# here iterated over both key and value
-    print(i,":",j)
+# for i,j in dict.items():# here iterated over both key and value
+#     print(i,":",j)
+
+
+import json 
+#write
+data = {"name":"uquiora","age":"unknown","life":"chill"}
+with open("experiment.json",'w') as file:
+    json.dump(data,file)
+
+# read
+with open("experiment.json" ,'r') as file:
+    data = json.load(file)
+print(data)
+
+#append
+with open("experiment.json" ,'r') as file:
+    Temp_data = json.load(file)
+new_data = {"name":"ragna","age":20,"life":"horrible"}
+data = {1:Temp_data,2:new_data}
+with open("experiment.json",'w') as file:
+    json.dump(data,file,indent=4)
+
+with open("experiment.json" ,'r') as file:
+    data = json.load(file)
+print(data)
--- self learning/python41.py
+++ self learning/python41.py
@@ -2,6 +2,13 @@
 a = 200
 b = 200
 print("yes") if a>b else print("no")
+
+# usually not recmmended to do
 print("yes") if a>b else print("no")if a == b else print("nt") # here inside else iam using another short hand if else
+
+
 c = 9 if a>b else 3
 print(c)
+strength = 300
+magic = 20 if strength > 400 else(_ for _ in ()).throw(Exception("your cannot increase your magic power unless you have strength above 400"))
+print(magic)
